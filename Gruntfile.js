e43a8771f9c01cd72aaafec339fb72261ced9d9a
JP Bochi <jpbochi@gmail.com>
2013-09-30 22:57:25 -0300

    fixed dependencies and added jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,19 @@
 module.exports = function (grunt) {
   'use strict';
 
+  var _ = require('lodash');
+  var globals = _.reduce([
+    'Kinetic',
+    '$',
+    'console', 'process',
+    'require', 'define',
+    'describe', 'it', 'beforeEach', 'afterEach',
+    'expect'
+  ], function (acc, name) {
+    acc[name] = true;
+    return acc;
+  }, {});
+
   grunt.initConfig({
     mochaTest: {
       test: {
@@ -10,11 +23,41 @@ module.exports = function (grunt) {
           reporter: 'spec' //'mocha-unfunk-reporter'
         }
       }
+    },
+    jshint: {
+      all: [
+        'lib/**/*.js',
+        'tests/**/*.js',
+        '*.js'
+      ],
+      options: {
+        indent: 2,
+        expr: true,
+        bitwise: true,
+        camelcase: true,
+        curly: true,
+        immed: true,
+        noarg: true,
+        nonew: true,
+        plusplus: true,
+        quotmark: 'single',
+        trailing: true,
+        maxparams: 4,
+        maxlen: 150,
+        browser: true,
+        nomen: false,
+        jquery: true,
+        white: true,
+        undef: true,
+        globals: globals
+      }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-mocha-test');
 
   grunt.registerTask('test', ['mochaTest']);
-  grunt.registerTask('default', ['test']);
+  grunt.registerTask('ci', ['jshint', 'test']);
+  grunt.registerTask('default', ['ci']);
 };
