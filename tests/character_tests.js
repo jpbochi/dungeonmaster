e7e2089799e5191fccae2dbe6e5c0e2a744aa245
JP Bochi <jpbochi@gmail.com>
2013-09-30 23:23:03 -0300

    a character has 6 abilities

--- tests/character_tests.js
+++ tests/character_tests.js
@@ -7,5 +7,20 @@ define(function (require) {
 
       expect(bob.race()).to.equal('human');
     });
+
+    describe('abilities', function () {
+      it('has strengh, constitution, dexterity, intelligence, wisdom, and charisma', function () {
+        var bob = character();
+
+        bob.abilities([16, 14, 13, 12, 11, 10]);
+
+        expect(bob.str()).to.equal(16);
+        expect(bob.con()).to.equal(14);
+        expect(bob.dex()).to.equal(13);
+        expect(bob.int()).to.equal(12);
+        expect(bob.wis()).to.equal(11);
+        expect(bob.cha()).to.equal(10);
+      });
+    });
   });
 });
