fa1d39c0edaff31b1efa618fcc3555631247a894
JP Bochi <jpbochi@gmail.com>
2013-10-01 01:13:38 -0300

    withAbilities is a mixin

--- tests/character_tests.js
+++ tests/character_tests.js
@@ -45,55 +45,22 @@ define(function (require) {
       });
     });
 
-    describe('abilities', function () {
-      it('has strengh, constitution, dexterity, intelligence, wisdom, and charisma', function () {
-        var bob = character();
+    it('has abilities', function () {
+      var bob = character();
 
-        bob.abilities([16, 14, 13, 12, 11, 10]);
+      expect(bob.str).to.instanceOf(Function);
+      expect(bob.con).to.instanceOf(Function);
+      expect(bob.dex).to.instanceOf(Function);
+      expect(bob.int).to.instanceOf(Function);
+      expect(bob.wis).to.instanceOf(Function);
+      expect(bob.cha).to.instanceOf(Function);
 
-        expect(bob.str()).to.equal(16);
-        expect(bob.con()).to.equal(14);
-        expect(bob.dex()).to.equal(13);
-        expect(bob.int()).to.equal(12);
-        expect(bob.wis()).to.equal(11);
-        expect(bob.cha()).to.equal(10);
-      });
-
-      it('can be set individually', function () {
-        var bob = character();
-
-        expect(bob.str(10)).to.equal(10);
-        expect(bob.con(11)).to.equal(11);
-        expect(bob.dex(12)).to.equal(12);
-        expect(bob.int(13)).to.equal(13);
-        expect(bob.wis(14)).to.equal(14);
-        expect(bob.cha(16)).to.equal(16);
-      });
-
-      describe('modifiers', function () {
-        [
-          { abl:  1, mod: -5 },
-          { abl:  2, mod: -4 }, { abl: 3, mod: -4 },
-          { abl:  4, mod: -3 }, { abl: 5, mod: -3 },
-          { abl:  6, mod: -2 }, { abl: 7, mod: -2 },
-          { abl:  8, mod: -1 }, { abl: 9, mod: -1 },
-          { abl: 10, mod: 0 }, { abl: 11, mod: 0 },
-          { abl: 12, mod: 1 }, { abl: 13, mod: 1 },
-          { abl: 14, mod: 2 }, { abl: 15, mod: 2 },
-          { abl: 16, mod: 3 }, { abl: 17, mod: 3 },
-          { abl: 18, mod: 4 }, { abl: 19, mod: 4 }
-        ].forEach(function (test) {
-          it('is ' + test.mod + ' for ability score of ' + test.abl, function () {
-            var bob = character();
-
-            bob.str(test.abl);
-            expect(bob.strMod()).to.equal(test.mod);
-
-            bob.dex(test.abl);
-            expect(bob.dexMod()).to.equal(test.mod);
-          });
-        });
-      });
+      expect(bob.strMod).to.instanceOf(Function);
+      expect(bob.conMod).to.instanceOf(Function);
+      expect(bob.dexMod).to.instanceOf(Function);
+      expect(bob.intMod).to.instanceOf(Function);
+      expect(bob.wisMod).to.instanceOf(Function);
+      expect(bob.chaMod).to.instanceOf(Function);
     });
   });
 });
---
+++ tests/with_abilities_tests.js
@@ -0,0 +1,54 @@
+define(function (require) {
+  var withAbilities = require('lib/with_abilities.js');
+
+  describe('withAbilities', function () {
+    it('has strengh, constitution, dexterity, intelligence, wisdom, and charisma', function () {
+      var bob = withAbilities({});
+
+      bob.abilities([16, 14, 13, 12, 11, 10]);
+
+      expect(bob.str()).to.equal(16);
+      expect(bob.con()).to.equal(14);
+      expect(bob.dex()).to.equal(13);
+      expect(bob.int()).to.equal(12);
+      expect(bob.wis()).to.equal(11);
+      expect(bob.cha()).to.equal(10);
+    });
+
+    it('can be set individually', function () {
+      var bob = withAbilities({});
+
+      expect(bob.str(10)).to.equal(10);
+      expect(bob.con(11)).to.equal(11);
+      expect(bob.dex(12)).to.equal(12);
+      expect(bob.int(13)).to.equal(13);
+      expect(bob.wis(14)).to.equal(14);
+      expect(bob.cha(16)).to.equal(16);
+    });
+
+    describe('modifiers', function () {
+      [
+        { abl:  1, mod: -5 },
+        { abl:  2, mod: -4 }, { abl: 3, mod: -4 },
+        { abl:  4, mod: -3 }, { abl: 5, mod: -3 },
+        { abl:  6, mod: -2 }, { abl: 7, mod: -2 },
+        { abl:  8, mod: -1 }, { abl: 9, mod: -1 },
+        { abl: 10, mod:  0 }, { abl: 11, mod: 0 },
+        { abl: 12, mod:  1 }, { abl: 13, mod: 1 },
+        { abl: 14, mod:  2 }, { abl: 15, mod: 2 },
+        { abl: 16, mod:  3 }, { abl: 17, mod: 3 },
+        { abl: 18, mod:  4 }, { abl: 19, mod: 4 }
+      ].forEach(function (test) {
+        it('is ' + test.mod + ' for ability score of ' + test.abl, function () {
+          var bob = withAbilities({});
+
+          bob.str(test.abl);
+          expect(bob.strMod()).to.equal(test.mod);
+
+          bob.dex(test.abl);
+          expect(bob.dexMod()).to.equal(test.mod);
+        });
+      });
+    });
+  });
+});
