9323644fdb9a0317029be7e897d4f699b75f123f
JP Bochi <jpbochi@gmail.com>
2013-10-02 01:52:05 -0300

    moved jshint config to .jshintrc

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,20 +2,11 @@
 module.exports = function (grunt) {
   'use strict';
 
-  var _ = require('lodash');
-  var globals = _.reduce([
-    'Kinetic',
-    '$',
-    'console', 'process',
-    'require', 'define',
-    'describe', 'it', 'beforeEach', 'afterEach',
-    'expect'
-  ], function (acc, name) {
-    acc[name] = true;
-    return acc;
-  }, {});
+  var jshintConfig = require('fs').readFileSync('./.jshintrc', { encoding: 'utf8' });
+  jshintConfig = JSON.parse(jshintConfig);
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     mochaTest: {
       test: {
         src: [ 'tests/all.js' ],
@@ -30,27 +21,7 @@ module.exports = function (grunt) {
         'tests/**/*.js',
         '*.js'
       ],
-      options: {
-        indent: 2,
-        expr: true,
-        bitwise: true,
-        camelcase: true,
-        curly: true,
-        immed: true,
-        noarg: true,
-        nonew: true,
-        plusplus: true,
-        quotmark: 'single',
-        trailing: true,
-        maxparams: 4,
-        maxlen: 150,
-        browser: true,
-        nomen: false,
-        jquery: true,
-        white: true,
-        undef: true,
-        globals: globals
-      }
+      options: jshintConfig
     }
   });
 
