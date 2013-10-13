e1f96a4984c920b5944ef917e67695cfa35906cd
JP Bochi <jpbochi@thoughtworks.com>
2013-10-13 16:50:50 -0300

    added grunt task to ensure no .only filter is forgot on tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,7 +28,32 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-mocha-test');
 
+  var exec = require('child_process').exec;
+  grunt.registerTask('check_tests', 'Search for temporary filters on tets.', function () {
+    var done = this.async();
+    var child = exec(
+      "git grep '\\.only(' ./tests/ || :",
+      { timeout: 9000 },
+      function (error, stdout, stderr) {
+        if (stdout) {
+          console.log('Found filtered tests.\n' + stdout);
+          return done(false);
+        }
+        if (stderr) {
+          console.log('Failed to run `git grep`.\n' + stderr);
+          done(false);
+        }
+        if (error) {
+          console.log('Command exited with "' + error.code + '", signal "' + error.signal + '".');
+          console.log(error);
+          done(false);
+        }
+        done(true);
+      }
+    );
+  });
+
   grunt.registerTask('test', ['mochaTest']);
-  grunt.registerTask('ci', ['jshint', 'test']);
+  grunt.registerTask('ci', ['jshint', 'test', 'check_tests']);
   grunt.registerTask('default', ['ci']);
 };
